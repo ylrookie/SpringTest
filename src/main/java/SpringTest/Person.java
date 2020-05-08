1b08521dfa23cf1494ad4bceed0c0f2d822c7f3b
tom <792932348@qq.com>
2020-05-08 21:23:17 +0800

    Spring and Maven first commit

---
+++ src/main/java/SpringTest/Person.java
@@ -0,0 +1,25 @@
+package SpringTest;
+
+public class Person {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    //自定义方法
+    public void sayHello(){
+        System.out.println("My Name is " + name);
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                '}';
+    }
+}
