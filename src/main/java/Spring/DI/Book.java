50fa6a17aa27df997affa5355727e9dddbd7f9b2
tom <792932348@qq.com>
2020-05-15 18:18:44 +0800

    p命名和字面量

---
+++ src/main/java/Spring/DI/Book.java
@@ -0,0 +1,30 @@
+package Spring.DI;
+
+public class Book {
+    private Integer bookId;
+    private String  bookName;
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "bookId=" + bookId +
+                ", bookName='" + bookName + '\'' +
+                '}';
+    }
+
+    public Integer getBookId() {
+        return bookId;
+    }
+
+    public void setBookId(Integer bookId) {
+        this.bookId = bookId;
+    }
+
+    public String getBookName() {
+        return bookName;
+    }
+
+    public void setBookName(String bookName) {
+        this.bookName = bookName;
+    }
+}
--- src/main/java/Spring/DI/TestDI.java
+++ src/main/java/Spring/DI/TestDI.java
@@ -19,11 +19,24 @@ public class TestDI {
         //1、创建容器对象(只要创建IOC容器队形，所有的bean都会创建)
         ApplicationContext app = new ClassPathXmlApplicationContext("spring-di.xml");
         //2、获取Car对象
-        Car car1 = app.getBean("car1",Car.class);
+        //Car car1 = app.getBean("car",Car.class);
 
-        Car car2 = app.getBean("car2",Car.class);
+        Car car2 = app.getBean("car1",Car.class);
         //3、输出对象
-        System.out.println(car1);
+        //System.out.println(car1);
         System.out.println(car2);
+    }
+    @Test
+    public void testPnameSpace(){
+        ApplicationContext app = new ClassPathXmlApplicationContext("spring-di.xml");
+        Car car3 = app.getBean("car3",Car.class);
+        System.out.println(car3);
+    }
+
+    @Test
+    public void testZML(){
+        ApplicationContext app = new ClassPathXmlApplicationContext("spring-di.xml");
+        Book book = app.getBean("book",Book.class);
+        System.out.println(book);
     }
 }
